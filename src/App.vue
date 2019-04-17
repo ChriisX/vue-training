29076a9618aba1cc4671497c6f2b2c250600c84f
Christophe Chichmanian <chris.chich@hotmail.fr>
2019-04-17 22:53:54 +0200

    :sparkles: Added a basic component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BasicComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BasicComponent from './components/BasicComponent.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BasicComponent
   }
 }
 </script>
---
+++ src/components/BasicComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span v-bind:title="title">{{ message }}</span>
+    <span v-if="visible">This message is visible</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue!",
+      title: "You loaded this page on " + new Date().toLocaleString(),
+      visible: true
+    };
+  }
+};
+</script>
+
+<style>
+span {
+  display: block;
+}
+</style>
