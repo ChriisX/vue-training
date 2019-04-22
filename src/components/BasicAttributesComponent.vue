b18b318e3702363d75634fdce1701d5ab6a173df
Christophe Chichmanian <chris.chich@hotmail.fr>
2019-04-22 16:50:56 +0200

    :sparkles: Added attributes examples

--- src/components/BasicAttributesComponent.vue
+++ src/components/BasicAttributesComponent.vue
@@ -7,6 +7,16 @@
       <li>With v-once: <span v-once>{{ value }}</span></li>
       <li>Without v-once: <span>{{ value }}</span></li>
     </ul>
+    <h2>v-html</h2>
+    <ul>
+      <li>With v-html: <span v-html="rawHTML"></span></li>
+      <li>Without v-html: <span>{{ rawHTML }}</span></li>
+    </ul>
+    <h2>v-bind</h2>
+    <button :disabled="disabled">Disabled button</button>
+    <h2>v-if / ternary</h2>
+    <button @click="toggle">Toggle {{ display ? 'off' : 'on'}}</button><br />
+    <span v-if="display">I'm a light and I'm on</span>
   </div>
 </template>
 
@@ -15,12 +25,18 @@ export default {
   name: "BasicAttributesComponent",
   data() {
     return {
-      value: 0
+      value: 0,
+      rawHTML: "<strong>text</strong>",
+      disabled: true,
+      display: true
     }
   },
   methods: {
     increment: function() {
       this.value++;
+    },
+    toggle: function() {
+      this.display = !this.display;
     }
   }
 };
@@ -30,5 +46,10 @@ export default {
 ul {
   list-style-type: none;
   padding: 0;
+}
+
+span {
+  display: inline-block;
+  margin: 5px;
 }
 </style>
